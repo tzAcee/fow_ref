7b6d7dee3d07998c3def1035c5d7fd797fbbeb29
Maxim Leis <maximleis3@gmail.com>
2020-05-14 12:22:38 +0200

    Charge now in Price Class

--- src/Movie.java
+++ src/Movie.java
@@ -3,19 +3,31 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
 
-    public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        priceCode = newpriceCode;
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -23,30 +35,13 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double charge = 0;
-        switch (priceCode) {
-            case Movie.REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoint(int daysRented) {
         // add bonus for a two day new release rental
-        if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
         return 1;
     }
-
 
 }
