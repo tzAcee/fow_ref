72a7ee1af39d857be5b89cc0df997f6820b769b6
Maxim Leis <maximleis3@gmail.com>
2020-05-14 12:17:06 +0200

    getCharge now in Movie Class

--- src/Movie.java
+++ src/Movie.java
@@ -22,4 +22,24 @@ public class Movie {
         return title;
     }
 
+    double getCharge(int daysRented) {
+        double charge = 0;
+        switch (priceCode) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+        }
+        return charge;
+    }
+
 }
--- src/Rental.java
+++ src/Rental.java
@@ -16,23 +16,7 @@ class Rental {
     }
 
     public double getCharge() {
-        double amount = 0.0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoint()
