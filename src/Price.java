dadfe00143bfc21096e98086ecddb1d569990125
Maxim Leis <maximleis3@gmail.com>
2020-05-14 12:25:49 +0200

    FrequentRenterPoints now polymorphed

--- src/Price.java
+++ src/Price.java
@@ -1,15 +1,19 @@
 abstract class Price {
-    abstract int getPriceCode();
+    public abstract int getPriceCode();
 
 
-    abstract double getCharge(int daysRented);
+    public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
 
 class ChildrensPrice extends Price {
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.CHILDRENS;
     }
-    double getCharge(int daysRented) {
+    public double getCharge(int daysRented) {
         double result = 1.5;
         if (daysRented > 3)
             result += (daysRented - 3) * 1.5;
@@ -18,19 +22,24 @@ class ChildrensPrice extends Price {
 }
 
 class NewReleasePrice extends Price {
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
-    double getCharge(int daysRented) {
+    public double getCharge(int daysRented) {
         return daysRented * 3;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add bonus for a two day new release rental
+        return (daysRented > 1) ? 2 : 1;
     }
 }
 
 class RegularPrice extends Price {
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.REGULAR;
     }
-    double getCharge(int daysRented) {
+    public double getCharge(int daysRented) {
         double result = 2;
         if (daysRented > 2)
             result += (daysRented - 2) * 1.5;
